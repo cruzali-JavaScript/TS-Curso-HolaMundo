4f7e740617a1f365e0ea9874305a450e1a562e9e
Ali <ali.cruz@me.com>
2023-05-07 18:23:41 -0600

    Carga inicial

---
+++ dist/index.js
@@ -0,0 +1,42 @@
+"use strict";
+let mensaje = "Hola Mundo...";
+mensaje = "Nuevo string";
+console.log(mensaje);
+mensaje = "chao mundo";
+let extincionDinosaurios = 76000000;
+let dinosaurioFavorito = "T-Rex";
+let extinto = true;
+let otraVariable = 33;
+let variableAny;
+variableAny = "hola";
+variableAny = 33;
+function funcion_ali(conf) {
+    return conf;
+}
+let animales = ['animal1', 'animal2', 'animal3'];
+let nums = [1, 2, 3];
+let checks = [];
+let nums2 = [];
+let tupla1 = [1, ['nombre1', 'otro']];
+tupla1.push(5, ['ss']);
+const chica = 's';
+const mediana = 'm';
+const grande = 'l';
+const extragrande = 'xl';
+var Talla;
+(function (Talla) {
+    Talla[Talla["Chica"] = 6] = "Chica";
+    Talla[Talla["Mediana"] = 7] = "Mediana";
+    Talla[Talla["Grande"] = 8] = "Grande";
+    Talla[Talla["ExtraGrande"] = 9] = "ExtraGrande";
+})(Talla || (Talla = {}));
+const variableTalla = Talla.Grande;
+console.log(variableTalla);
+const estado = 2;
+const objeto = {
+    id: 1,
+    nombre: 'Ali',
+    talla: Talla.Chica
+};
+objeto.nombre = 'Hola Mundo';
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,71 @@
+let mensaje: string = "Hola Mundo..."
+
+mensaje = "Nuevo string"
+console.log(mensaje)
+
+mensaje = "chao mundo"
+
+let extincionDinosaurios: number = 76_000_000
+let dinosaurioFavorito: string = "T-Rex"
+let extinto: boolean = true
+
+// extinto = 44 // Ya se especifico como boolean
+
+let otraVariable = 33
+
+// otraVariable = "mas" // Ya se inicializo como int
+
+
+let variableAny
+variableAny = "hola"
+variableAny = 33
+
+
+function funcion_ali(conf: any)
+{
+    return conf
+}
+
+
+let animales: string[] = ['animal1', 'animal2', 'animal3']
+let nums: number[] = [1,2,3]
+let checks: boolean[] = []
+let nums2: Array<number> = []
+
+// animales.map(X => X.) // El autocompletado sugiere métodos del tipo de dato.
+
+// Tuplas
+
+let tupla1: [number, string[]] = [1, ['nombre1','otro']]
+tupla1.push(5, ['ss'])
+
+
+const chica = 's'
+const mediana = 'm'
+const grande = 'l'
+const extragrande = 'xl'
+
+enum Talla {Chica = 6, Mediana, Grande, ExtraGrande}
+
+const variableTalla = Talla.Grande
+console.log(variableTalla)
+
+
+const enum LoadingState {Idle, Loading, Success, Error}
+
+const estado = LoadingState.Success
+
+type Persona = {
+    readonly id: number,
+    nombre?: string,
+    talla: Talla
+    }
+
+const objeto: Persona = {
+    id: 1,
+    nombre: 'Ali',
+    talla: Talla.Chica
+}
+
+objeto.nombre = 'Hola Mundo'
+// objeto.id = 42 // Da error, la propiedad es de solo lectura
